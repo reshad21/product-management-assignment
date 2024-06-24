69942b1b69aef1216999ee3bfdfbe8df67c98c12
reshad21 <miakala7@gmail.com>
2024-06-24 11:07:07 +0600

    create not found route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // const express = require('express')
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import notFound from './app/middlewares/notFound';
 import { OrderRoutes } from './app/modules/order/order.route';
 import { ProductRoutes } from './app/modules/product/product.route';
 const app: Application = express()
@@ -19,5 +20,6 @@ app.use('/api/v1/orders', OrderRoutes)
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
-
+//Not Found
+app.use(notFound);
 export default app;
---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+
+
+import { NextFunction, Request, Response } from 'express';
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+    return res.status(400).json({
+        success: false,
+        message: 'API Not Found !!',
+        error: '',
+    });
+};
+
+export default notFound;
